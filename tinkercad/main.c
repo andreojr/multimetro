d4942a033ccba1baee7afbb3a82cbf21c3cb7d4d
André Júnior <atipicode.ec@gmail.com>
2024-11-16 20:19:05 -0300

    tinkercad mvp

---
+++ tinkercad/main.c
@@ -0,0 +1,119 @@
+#include <Adafruit_LiquidCrystal.h>
+
+Adafruit_LiquidCrystal lcd_1(0);
+const int BUTTON = 2;
+const int VOLTAGE = 13;
+const int RESISTANCE = 12;
+const int CURRENT = 8;
+const int AMPLIFIER = 4;
+const float MAX_VOLTAGE = 5.0;
+int mode = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd_1.begin(16,2);
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(VOLTAGE, OUTPUT);
+  pinMode(CURRENT, OUTPUT);
+  pinMode(AMPLIFIER, OUTPUT);
+  pinMode(RESISTANCE, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(BUTTON), changeMode, FALLING);
+}
+
+void loop()
+{ 
+  digitalWrite(VOLTAGE, mode == 0);
+  digitalWrite(CURRENT, mode == 1);
+  digitalWrite(AMPLIFIER, mode == 1);
+  digitalWrite(RESISTANCE, mode == 2);
+  
+  run();
+  
+  delay(1000);
+  lcd_1.clear();
+}
+
+void run() {
+  switch (mode) {
+    case 0:
+      setVoltageMode();
+      break;
+    case 1:
+      setCurrentMode();
+      break;
+    case 2:
+      setResistanceMode();
+      break;
+  }
+}
+
+void printd(float value, String unit) {
+  String message = String(value) + " " + unit;
+  lcd_1.print(message);
+}
+
+void changeMode() {
+	mode = (mode + 1) % 3;
+}
+
+void setVoltageMode() {
+  lcd_1.setCursor(0,0);
+  lcd_1.print("Voltage");
+  lcd_1.setCursor(0,1);
+  
+  printd(getVoltage(), "V");
+}
+
+float getVoltage() {
+  const float a2 = analogRead(2);
+  const float a3 = analogRead(3);
+  return getVoltage(a3, a2);
+}
+
+float getVoltage(float pos, float neg) {
+  // Calcula a tensao
+  float voltage = (pos - neg) * (MAX_VOLTAGE / 1023);
+  return voltage;
+}
+                  
+void setCurrentMode() {
+  lcd_1.setCursor(0,0);
+  lcd_1.print("Current");
+  lcd_1.setCursor(0,1);
+  printd(getCurrent(), "mA");
+}
+
+float getCurrent() {
+  // Obtem a tensao amplificada
+  const float a0 = analogRead(0);
+  const float a1 = analogRead(1);
+  float voltage = getVoltage(a1, a0);
+  Serial.println(voltage);
+  // Declara resistor shunt
+  const float shunt = 1;
+  // Calcula a corrente
+  float current = voltage / shunt;
+  // Retorna a corrente em mA
+  return current;
+}
+
+void setResistanceMode() {
+  lcd_1.setCursor(0,0);
+  lcd_1.print("Resistance");
+  lcd_1.setCursor(0,1);
+  printd(getResistance(), "kO");
+}
+
+float getResistance() {
+  // Obtem a tensao
+  float voltage = getVoltage();
+  Serial.println(voltage);
+  // Declara resistor shunt
+  const float shunt = 1000;
+  // Calcula a resistencia
+  float resistance = voltage / (MAX_VOLTAGE - voltage) * shunt;
+  // Retorna a resistencia em kOhms
+  return resistance / 1000;
+}
